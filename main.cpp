902f44cec4addf799ace8822c3ef4d6539d47675
Dan-k391 <3242118462@qq.com>
2023-03-21 00:22:19 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
 // TODO: 1. Change to standard cpp project structure in future
 // TODO: 2. Use cmake
 
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Verifier.h"
-
 #include <cassert>
 #include <cstdio>
 #include <iostream>
